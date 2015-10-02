b5024dd068b99cbc1d457cb5ed61c539c949b8b1
Arne <a.chr.hansen@gmx.de>
2015-10-02 09:46:42 +0200

    add svg generator from DB

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ error_reporting(E_ALL);
 <link rel="stylesheet" href="style.css">
 </head>
 
-<!--<body>
+<body>
 <div id="header">
-        <img src="img/img_2780.jpg"  alt="icits">
-</div>-->
+        <img src="img/header.jpg" width="100%" alt="wall">
+</div>
 <div>
 <?php
 $con = mysqli_connect('localhost','climber','Cornaredo','cornaredo_routes');
@@ -26,7 +26,7 @@ if (!$con)
 $result = mysqli_query($con,"SELECT * FROM grades");
 
 echo '<select name="bcls" id="bcls">';
-echo '<option value="">Select a route</option>';
+echo '<option value="">Grade</option>';
 
 while($row = mysqli_fetch_array($result))
 {
@@ -36,6 +36,57 @@ while($row = mysqli_fetch_array($result))
 echo '</select>';
 ?>
 </div>
+<div id="tableOutput">
+  <?php
+    $con = mysqli_connect('localhost','climber','Cornaredo','cornaredo_routes');
+    if (!$con)
+    {
+      die('Could not connect' .  mysqli_error($con));
+    }
+    $result_gen = mysqli_query($con, "SELECT * FROM routes" );
 
+    echo '<table id="output">';
+    echo '<tr id="title"><td>Route Name</td><td>Grade</td><td>Creator</td></tr>';
+
+    while( $row = mysqli_fetch_array($result_gen))
+    {
+      $grade = mysqli_fetch_assoc(mysqli_query( $con, "SELECT difficulty FROM grades WHERE grades.d_value = " . $row['difficulty']));
+      echo '<tr id="' . $row['id'] . '" class="row"><td>';
+      echo $row['name'] . '</td><td>'  . $grade['difficulty'] . '</td><td>' . $row['creator'] . '</td></tr>';
+    }
+
+        echo '</table>';
+  ?>
+
+</div>
+<div id="route_picture">
+  <?php
+    $con = mysqli_connect('localhost','climber','Cornaredo','cornaredo_routes');
+    if (!$con)
+    {
+      die('Could not connect' .  mysqli_error($con));
+    }
+    $result_gen = mysqli_query($con, "SELECT * FROM routes" );
+    
+    while( $row = mysqli_fetch_array($result_gen))
+    {
+      $img_size = getimagesize( 'img/'.$row['p1']);
+      echo '<svg height="'. $img_size[1].'" width="'. $img_size[0] .'">';
+      echo '<image id="testimg1" xlink:href="img/' . $row['p1']. '" width="' . $img_size[0] . '" height="' . $img_size[1] . '" x="0" y="0"/>';
+      echo '<g stroke="red" stroke-width="3" fill="red">';
+      for ( $i=1; $i <= 20; $i +=1) {
+        //echo $i . '  ';
+        //echo $row[4+$i] . '  ';
+        //echo $row[24+$i] . '  ';
+        if ( $row[ 4 + $i ] != NULL AND $row[24 + $i] != NULL ) {
+          echo '<circle id="p' . $i . '" cx="' . $row[4+$i] . '" cy="' . $row[24+$i] . '" r="3" />';
+          echo '<text x="' . $row[4+$i] . '" y="' . $row[24+$i] . '" dy="+20">' . $i . '</text>';
+        } 
+      }
+      echo '</g>';
+      echo '</svg>';
+    }
+  ?>
+</div>
 </body>
 </html>
